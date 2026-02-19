95b109b76093c24862ee78eb60f8997c0753693e
Ivan Irakoze <134937903+irakoze-io@users.noreply.github.com>
2026-02-20 00:23:04 +0200

    Data Initialization inception

--- pgsql/docker/postgres/init/03-init-nirva.sql
+++ pgsql/docker/postgres/init/03-init-nirva.sql
@@ -4,6 +4,7 @@ CREATE DATABASE nirva;
 
 create table customers (
     customerId bigint primary key,
+    account varchar(50) unique not null,
     firstName varchar(100) not null,
     lastName varchar(100) not null,
     msisdn varchar(20) unique not null,
@@ -12,15 +13,22 @@ create table customers (
     homeAddress text,
     city varchar(100),
     postalCode varchar(20),
-    houseHoldId varchar(50)
+    houseHoldId varchar(50),
+    createdAt timestamp with time zone default now()
 );
 
-COPY customers (customerId, firstName, lastName,
+create index idx_customers_msisdn on customers(msisdn);
+create index idx_customers_customerId on customers(customerId);
+create index idx_customers_houseHoldId on customers(houseHoldId);
+
+COPY customers (
+    customerId, account, firstName, lastName,
     msisdn,
     gender,
     dateOfBirth,
     homeAddress,
     city,
     postalCode,
-    houseHoldId)
+    houseHoldId
+    )
 FROM '/docker-entrypoint-initdb.d/data/customers.csv' DELIMITER ',' CSV HEADER;
